8c5d8d0306923367c7e31df6f2a8b46476956b84
Kirsten <kirstenmhattori@gmail.com>
2022-08-25 17:29:50 -0600

    finished HTML, CSS, & JS for nav slideout & bar for mobile and larger screens

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburgerButton = document.querySelector('#hamburger');
   const menuCloseButton = document.querySelector('#closeMenu');
-  const menu = document.querySelector('.slideOutNav');
+  const menu = document.querySelector('.slideOut');
 
   hamburgerButton.addEventListener('click', (e) => {
     menu.classList.add('open');
