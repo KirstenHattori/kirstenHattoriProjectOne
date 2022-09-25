77ae7592d311223de35b34b25c353f3c287cf48d
Kirsten <kirstenmhattori@gmail.com>
2022-09-25 15:19:52 -0700

    working on the namespace object and date method

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ document.addEventListener("DOMContentLoaded", () => {
     menu.classList.remove('open');
   });
 })
+
+// BLOG FORM
+// ***DOING RIGHT MEOW! === defining the variables outside of the date() method and as properties of the blog {}
+const blog = {};
+// event listening and handling of the form 
+  // tracking user inputs of name and comment 
+  // on submit, date() is called 
+
+  blog.dateString = Date();
+  blog.dateArray // *** */
+
+  
+
+    // turn the string into an array so can access individual values and use them to populate the page ( ${} style)
+    let dateArray = [];
+    dateArray = dateString.split(' ');
+    // need to convert the three letters for dayofweek and month to full words to be displayed on page. = updating the value of dayOfWeek and month
+    // conditional of an if (dayOfWeek = Sun) {dayOfWeek = Sunday} else if (dayOfWeek = Mon){dayOfWeek = Monday} etc
+    // extract that out to a fucntion that accepts dayOfWeek as an argument
+    dayOfWeek = dateArray[0];
+    month = dateArray[1];
+    dayOfMonth = dateArray[2];
+    year = dateArray[3];
+
+    blog.date = function () {
+      // call Date() to get the datestamp of user submission
+      Date();
+    }
+// 
+// 
+//   
+// 
+
