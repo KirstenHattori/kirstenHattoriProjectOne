5c3902e516a04f3a6277976d0378e3e6fa98722d
Kirsten <kirstenmhattori@gmail.com>
2022-09-26 00:25:38 -0700

    preventing default form submit behaviour and capturing the value of the users inputs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// MAIN NAV
 document.addEventListener("DOMContentLoaded", () => {
   const hamburgerButton = document.querySelector('#hamburger');
   const menuCloseButton = document.querySelector('#closeMenu');
@@ -13,32 +14,89 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 // BLOG FORM
-// ***DOING RIGHT MEOW! === defining the variables outside of the date() method and as properties of the blog {}
-const blog = {};
-// event listening and handling of the form 
-  // tracking user inputs of name and comment 
-  // on submit, date() is called 
 
-  blog.dateString = Date();
-  blog.dateArray // *** */
+// ***DOING RIGHT MEOW! 
+  //=== defining the variables outside of the date() method and as properties of the blog {}
+
+// // creating namespace
+const blog = {};
+
+// event listening and handling of the form:
+  // target the form element and save as a variable so it is easier to use later 
+    // need to target by classname because there multiple forms on the pages that use this script file. 
+const form = document.querySelector('form');
+const forms = document.querySelectorAll('form');
+
+//   // attach event listener to the form
+  // create a function to be called when the form is submitted
+function preventDefault(event) {
+  // prevent the default behaviour of a form submmission
+  event.preventDefault();
+  console.log('hi');
+}
+
+// target the user inputs from the form and capturing their value in a variable
+const inputs = document.querySelectorAll('input');
+const commenterName = inputs[0].value;
+const comment = document.querySelector('textarea').value;
+
+// target the html elements to create new comment li's
+
+
+
+// function for updating textContent of html of li's in comment section
+  // to be called on submit
+
+
+
+
+
+
+
+// when there is just one function to callback
+  //  // addeventlistener to form and call the prevent default method 
+  // form.addEventListener('submit', preventDefault);
+
+// calling a bunch of outside declared functions
+form.addEventListener('submit', function(event){
+  preventDefault(event);
+
+})
+
+// form.addEventListener('submit', function(event){
+//   event.preventDefault();
+//   // blog.form.name = document.querySelector('input').value;
+//   // console.log(blog.form.name);
+//   console.log('hi');
+
+// });
+  
+// tracking user inputs of name and comment 
+  // // name input:
+  // blog.form.name = document.querySelector('input[]:')
 
   
-
-    // turn the string into an array so can access individual values and use them to populate the page ( ${} style)
-    let dateArray = [];
-    dateArray = dateString.split(' ');
+    // should this be declared inside the date method?
+  blog.date = function () {
+    // call Date() to get the datestamp of user submission
+    // Date();
+    // on submit, date() is called and the return is saved as dateString
+    blog.dateString = Date();
+    
+    // turn the string into an array so can access individual values and use them to populate the page ( ${}-style )
+    blog.dateArray = [];
+    blog.dateArray = blog.dateString.split(' ');
+    
     // need to convert the three letters for dayofweek and month to full words to be displayed on page. = updating the value of dayOfWeek and month
     // conditional of an if (dayOfWeek = Sun) {dayOfWeek = Sunday} else if (dayOfWeek = Mon){dayOfWeek = Monday} etc
     // extract that out to a fucntion that accepts dayOfWeek as an argument
-    dayOfWeek = dateArray[0];
-    month = dateArray[1];
-    dayOfMonth = dateArray[2];
-    year = dateArray[3];
+    blog.dayOfWeek = blog.dateArray[0];
+    blog.month = blog.dateArray[1];
+    blog.dayOfMonth = blog.dateArray[2];
+    blog.year = blog.dateArray[3];
+  }
 
-    blog.date = function () {
-      // call Date() to get the datestamp of user submission
-      Date();
-    }
+  
 // 
 // 
 //   
